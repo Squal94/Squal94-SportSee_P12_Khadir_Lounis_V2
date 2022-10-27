04598ee87ff067192e0f390fcbf2fa6ed024c130
squal94 <lounis.khadir@wanadoo.fr>
2022-10-27 15:31:41 +0200

    Création du KeyData Affichage

---
+++ front/src/Components/modulesMain/ModuleKeyData.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ModuleKeyData = (props) => {
+  return (
+    <div className="containerModule">
+      <div className="containerModule__flag"></div>
+      <div className="containerModule__data"></div>
+    </div>
+  );
+};
+
+export default ModuleKeyData;
