7402a763774de4a9e585cfd81d3dd8c1c7f92bb8
squal94 <lounis.khadir@wanadoo.fr>
2022-11-03 13:16:23 +0100

    intensite fonctionnel

--- front/src/Components/modulesMain/Intensite.js
+++ front/src/Components/modulesMain/Intensite.js
@@ -9,25 +9,17 @@ import {
 } from "recharts";
 
 const Intensite = (props) => {
-  let test = props?.data?.kind.value;
-  let mockKind = Object.keys(test);
-  console.log(mockKind);
-  // let mock = props.data;
-  // console.log(mock);
-  // const [array, setArray] = useState();
+  const [mockData, setMockData] = useState();
+  useEffect(() => {
+    setMockData(props);
+  }, [props]);
+  let arrayTitle = [];
 
-  // useEffect(() => {
-  //   setArray(props.data?.kind);
-  //   arraykind(array);
-  // }, [props, array]);
-
-  // useEffect(() => {});
-
-  // function arraykind(test) {
-  //   let monObjet = test;
-  //   let monTableau = Object.keys(monObjet);
-  //   console.log(monTableau);
-  // }
+  function arraykind(object) {
+    arrayTitle = [];
+    let monObjet = object?.data?.kind;
+    Object.keys(monObjet || {}).map((i) => arrayTitle.push(monObjet[i]));
+  }
 
   // const source = {
   //   kind: {
@@ -65,8 +57,8 @@ const Intensite = (props) => {
   //     },
   //   ],
   // };
-  // let arrayTitle;
-  // let graphiqueEntrie = [];
+
+  let graphiqueEntrie = [];
 
   // function intensiteArray(object) {
   //   let test = object?.kind;
@@ -77,36 +69,39 @@ const Intensite = (props) => {
   //   console.log(arrayTitle);
   // }
 
-  // intensiteArray(mock);
-  // function IntensiteObject(object) {
-  //   intensiteArray(object);
-  //   // let n = 0;
-  //   // graphiqueEntrie = [];
-  //   // arrayTitle.map((name) => {
-  //   //   const objectPush = { value: object.data[n].value, kind: name };
-  //   //   graphiqueEntrie.push(objectPush);
-  //   //   n++;
-  //   // });
-  // }
+  //intensiteArray(mock);
+  function IntensiteObject(object) {
+    arraykind(object);
+    let n = 0;
+    graphiqueEntrie = [];
+    arrayTitle.map((name) => {
+      const objectPush = { value: object.data.data[n].value, kind: name };
+      graphiqueEntrie.push(objectPush);
+      n++;
+    });
+  }
+
+  IntensiteObject(mockData);
+  console.log(graphiqueEntrie);
   // IntensiteObject(mock);
 
-  // return (
-  //   <RadarChart
-  //     width={258}
-  //     height={263}
-  //     cx="50%"
-  //     cy="50%"
-  //     outerRadius="60%"
-  //     data={graphiqueEntrie}
-  //     style={{ backgroundColor: "#282D30" }}
-  //     fill="#FFFFFF"
-  //   >
-  //     <PolarGrid />
-  //     <PolarAngleAxis dataKey="kind" tick={{ fontSize: 12 }} />
-  //     <PolarRadiusAxis visibility={"hidden"} />
-  //     <Radar dataKey="value" fill="#FF0101" fillOpacity={0.7} />
-  //   </RadarChart>
-  // );
+  return (
+    <RadarChart
+      width={258}
+      height={263}
+      cx="50%"
+      cy="50%"
+      outerRadius="60%"
+      data={graphiqueEntrie}
+      style={{ backgroundColor: "#282D30" }}
+      fill="#FFFFFF"
+    >
+      <PolarGrid />
+      <PolarAngleAxis dataKey="kind" tick={{ fontSize: 12 }} />
+      <PolarRadiusAxis visibility={"hidden"} />
+      <Radar dataKey="value" fill="#FF0101" fillOpacity={0.7} />
+    </RadarChart>
+  );
 };
 
 export default Intensite;
